043bb7312bc7cbe1d591ec4ae9c78a4ef6741d71
Kiraschild <vedantp1210@gmail.con>
2024-03-21 15:44:47 +0530

    First Commit

---
+++ emp_engagement/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+# Create your models here.
+class user_credentials(models.Model):
+    username = models.CharField(max_length=100)
+    password = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.username
+    
+    
+class user_data(models.Model):
+    Username= models.CharField(max_length=30)
+    Password= models.CharField(max_length=15)
+    FirstName= models.CharField(max_length=100)
+    MiddleName= models.CharField(max_length=100, blank=True)
+    LastName = models.CharField(max_length=100)
+    Address= models.CharField(max_length=50)
+    City= models.CharField(max_length=50)
+    State= models.CharField(max_length=50)
+    Pincode= models.CharField(max_length=10)
+    Country= models.CharField(max_length=50)
+    DateofBirth= models.DateField()
+    Gender_choices=(
+        ('M', 'Male'),
+        ('F', 'Female'),
+        ('O', 'Other')
+    )
+    Gender= models.CharField(max_length=1, choices=Gender_choices)
+    Qualifications = models.CharField(max_length=255)
+    Position = models.CharField(max_length=100)
+    Department = models.CharField(max_length=100)
+    Reportsto= models.CharField(max_length=50,default= None)
+    is_user = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    Profilepic = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
---
+++ emp_engagement/views.py
@@ -0,0 +1,74 @@
+from emp_engagement.models import user_credentials
+
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.urls import reverse
+from .decorators import login_access_only
+
+# Create your views here.
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def login_user(request):
+    if request.method == "POST":
+        username=request.POST.get('username')
+        password=request.POST.get('password')
+        print("Username: ",username)
+        print("Password: ",password)
+        try:
+            user= user_credentials.objects.get(username=username)
+            print(user)
+            if user.password == password:
+                request.session['username'] = username
+                messages.success(request,"Successfully logged in!")
+                print("User logged in",user)
+                return redirect('index/')
+            else :
+                messages.error(request,"Check Credentials")
+                return render(request,'login.html')
+
+        except user_credentials.DoesNotExist:
+            messages.error(request, "Invalid username")
+            return redirect("/login")
+            
+    return render(request,'login.html')
+
+def register_user(request):
+    return render(request,'register.html')
+
+@login_access_only
+def logout_user(request):
+    logout(request)
+    messages.info(request, "Logged out Successfully!")
+    return redirect('/login')
+
+@login_access_only
+def index(request): 
+    return render(request,'index.html')
+
+@login_access_only
+def home(request): 
+    return render(request, 'home.html')
+
+@login_access_only
+def dashboard(request): 
+    return render(request, 'dashboard.html')
+
+@login_access_only
+def user(request): 
+    return render(request, 'user.html')
+
+@login_access_only
+def task(request): 
+    return render(request, 'task.html')
+
+@login_access_only
+def timesheet(request): 
+    return render(request, 'timesheet.html')
+
+@login_access_only
+def leave(request): 
+    return render(request, 'leave.html')
